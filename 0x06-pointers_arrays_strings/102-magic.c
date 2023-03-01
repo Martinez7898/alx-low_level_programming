d4e87158c82820d929592c2c70f1172fd9a648e0
Martinez7898 <Mijeledy@gmail.com>
2023-02-28 22:11:11 -0800

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program demonstrates a magic trick by using a pointer
+ * to assign a value to an array element without directly modifying the array.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+
+a[2] = 1024;
+p = &n;
+
+    /*
+     * Write your line of code here...
+     * Remember:
+     * - You are not allowed to use 'a'
+     * - You are not allowed to modify 'p'
+     * - Only one statement
+     * - You are not allowed to code anything else than this line of code
+     */
+
+*(p + 7) = 98;
+
+/* ...so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
+
+return (0);
+}
+
